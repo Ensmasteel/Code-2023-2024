bce59b6b79e4aed440f88dd85d6a2c2b22983e58
titofra <titouan821@gmail.com>
2024-05-07 16:50:17 +0200

    lidar

--- ESP32/src/main.cpp
+++ ESP32/src/main.cpp
@@ -4,7 +4,7 @@
 #include "Communication.h"
 #include "Message.h"
 #include "IDS.h"
-#include "ldlidar_driver_linux.h"
+#include "lidar.h"
 
 #define LED_PIN     18
 #define NUM_LEDS    41
@@ -17,12 +17,12 @@ LiquidCrystal_I2C lcd(0x27, 16, 2);
 Communication comTeensy = Communication(&Serial);
 Message teen_msg;
 
-uint64_t GetTimestamp(void) {
-  std::chrono::time_point<std::chrono::system_clock, std::chrono::nanoseconds> tp = 
-    std::chrono::time_point_cast<std::chrono::nanoseconds>(std::chrono::system_clock::now());
-  auto tmp = std::chrono::duration_cast<std::chrono::nanoseconds>(tp.time_since_epoch());
-  return ((uint64_t)tmp.count());
-}
+Lidar lidar(&Serial2);
+lidarFrame lidar_frame;
+float angle_step;
+uint16_t distance;
+float angle;
+unsigned int point_id;
 
 void setup() {
     Serial.begin(115200);
@@ -41,67 +41,28 @@ void setup() {
     }
     FastLED.show();
 
-    // ldlidar::LDLidarDriverLinuxInterface* lidar_drv = ldlidar::LDLidarDriverLinuxInterface::Create();
-    // lidar_drv->RegisterGetTimestampFunctional(std::bind(&GetTimestamp)); 
-    // lidar_drv->EnablePointCloudDataFilter(true);
-    // lidar_drv->Connect(ldlidar::LDType::LD_19);
-    // lidar_drv->WaitLidarComm(3500);
-    // lidar_drv->Start();
-
-    // ldlidar::Points2D laser_scan_points;
-    // int cnt = 100;
-    // while (ldlidar::LDLidarDriverLinuxInterface::Ok()) {
-    //     if ((cnt--) <= 0) {
-    //         lidar_drv->Stop();
-    //     }
-
-    //     switch (lidar_drv->GetLaserScanData(laser_scan_points, 1500)){
-    //         case ldlidar::LidarStatus::NORMAL:
-    //             //  output 2d point cloud data
-    // lcd.setCursor(0,1);
-    // lcd.print(String(laser_scan_points.size()));
-    //             // for (auto point : laser_scan_points) {
-    //             //     LOG_INFO_LITE("stamp(ns):%lu,angle:%f,distance(mm):%d,intensity:%d", 
-    //             //         point.stamp, point.angle, point.distance, point.intensity);
-    //             // }
-    //             break;
-    //         case ldlidar::LidarStatus::DATA_TIME_OUT:
-    //             lidar_drv->Stop();
-    //             exit(EXIT_FAILURE);
-    //             break;
-    //         case ldlidar::LidarStatus::DATA_WAIT:
-    //             break;
-    //         default:
-    //             break;
-    //     }
-
-    //     delay(166);  // sleep 166ms , 6hz
-    // }
-
-
-    // lidar_drv->Stop();
-    // lidar_drv->Disconnect();
-    // ldlidar::LDLidarDriverLinuxInterface::Destory(lidar_drv);
 }
 
 void loop() {
 
-    /* Update the Teensy communication */
+    /* Updates */
     comTeensy.update();
+    lidar.update();
 
     /* Handle the lidar messages */
-    // lidar_msg = "";
-    // chr_tmp = ' ';
-    // while (Serial2.available() && chr_tmp != '\n') {
-    //     chr_tmp = (char)Serial2.read();
-    //     lidar_msg += chr_tmp;
-    // }
-    // if (lidar_msg.length() > 0) {
-    //     uint8_t lidar_bytes [lidar_msg.length()];
-    //     lidar_msg.getBytes(lidar_bytes, lidar_msg.length());
-    //     if (lidar.Parse(lidar_bytes, lidar_msg.length())) {
-    //         lidar.AssemblePacket();
-    //     }
-    // }
+    if (lidar.waitingRX()) {
+        lidar_frame = lidar.peekOldestFrame();
+
+        angle_step = (float) (lidar_frame.end_angle - lidar_frame.start_angle) * 0.01f / (FRAME_NPOINT - 1.0f);
+        for (point_id = 0; point_id < FRAME_NPOINT; point_id++) {
+            distance = lidar_frame.data[point_id].distance;
+            // if (distance > 100 && distance < 300) {
+                angle = ((float) lidar_frame.start_angle + angle_step * (float) point_id) * DEG_TO_RAD;
+                Serial.println("> xy :" + String(distance / 1000.0f * std::cos(angle),3) + ":" + String(distance / 1000.0f * std::sin(angle),3) + "|xy");
+            // }
+        }
+
+        lidar.popOldestFrame();
+    }
 
 }
